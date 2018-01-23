c0dfd99e156b7509a668b3294cee5ea83e86d1af
정세훈 <a01085109637@gmail.com>
2018-01-23 13:39:18 +0900

    Scanner로 입력한값 출력

--- src/GitprojPractice/GitprojPractice.java
+++ src/GitprojPractice/GitprojPractice.java
@@ -1,9 +1,21 @@
 package GitprojPractice;
+import java.util.Scanner;
 
 public class GitprojPractice {
 
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
 		System.out.println("안녕");
+		
+		//입력
+		System.out.println("숫자를 입력하세요ㅕ");
+		int left= sc.nextInt();
+		System.out.println("숫자를 또 입력하세요");
+		int right= sc.nextInt();
+		//출력
+		System.out.println("입력한 숫자"+left+","+right);
+	
 	}
+	
 
 }
